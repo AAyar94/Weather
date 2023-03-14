2a2a7c37b6af41961df7352991519e361f442abc
Adem Ayar <a.ayar94@gmail.com>
2023-03-14 23:19:43 +0300

    Network Source fixed,Home-screen filed up

--- app/src/main/java/com/aayar94/weather/di/WeatherDataNetworkModule.kt
+++ app/src/main/java/com/aayar94/weather/di/WeatherDataNetworkModule.kt
@@ -1,6 +1,8 @@
 package com.aayar94.weather.di
 
+import com.aayar94.weather.data.remote.GeoLocationServiceAPI
 import com.aayar94.weather.data.remote.WeatherServiceAPI
+import com.aayar94.weather.util.Constant
 import com.aayar94.weather.util.Constant.Companion.BASE_URL
 import dagger.Module
 import dagger.Provides
@@ -11,6 +13,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -44,7 +47,8 @@ object WeatherDataNetworkModule {
 
     @Provides
     @Singleton
-    fun provideRetrofitInstance(
+    @Named("Weather")
+    fun provideRetrofitInstanceForWeather(
         okHttpClient: OkHttpClient,
         gsonConverterFactory: GsonConverterFactory
     ): Retrofit {
@@ -57,8 +61,34 @@ object WeatherDataNetworkModule {
 
     @Provides
     @Singleton
-    fun provideWeatherServiceAPI(retrofit: Retrofit): WeatherServiceAPI {
+    fun provideWeatherServiceAPI(
+        @Named("Weather")
+        retrofit: Retrofit
+    ): WeatherServiceAPI {
         return retrofit.create(WeatherServiceAPI::class.java)
+    }
+
+    @Provides
+    @Singleton
+    @Named("GeoLocation")
+    fun provideRetrofitInstanceForGeoLocation(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constant.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGeoLocationServiceAPI(
+        @Named("GeoLocation")
+        retrofit: Retrofit
+    ): GeoLocationServiceAPI {
+        return retrofit.create(GeoLocationServiceAPI::class.java)
     }
 
 }
